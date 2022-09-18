6cd8d6fb1158cbffcb0b1b38eb0d7e4959897adc
johanCarvajalAndes <j.carvajalm@uniandes.edu.co>
2022-09-17 20:00:56 -0500

    coneccion base de datos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductoEntity } from './producto/producto.entity';
 import { ProductoModule } from './producto/producto.module';
+import { TiendaEntity } from './tienda/tienda.entity';
 import { TiendaModule } from './tienda/tienda.module';
 
 @Module({
-  imports: [ProductoModule, TiendaModule],
+  imports: [
+    ProductoModule,
+    TiendaModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'admin123',
+      database: 'tiendas',
+      entities: [ProductoEntity, TiendaEntity],
+      dropSchema: true,
+      synchronize: true,
+      keepConnectionAlive: true
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
